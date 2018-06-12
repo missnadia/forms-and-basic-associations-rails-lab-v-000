d453376b7b913e06ae6ca36fd4cceab493dbca83
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-12 19:10:44 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -19,7 +19,7 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def note_contents=(content)
+  def note_contents=(notes)
     self.notes = Note.find_or_create_by(content: content)
   end
 
